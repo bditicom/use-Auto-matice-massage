1044138b02afb1fc26030abfcd0233405675656a
“bditicom” <bditi.com@gmail.com>
2021-04-27 02:37:34 -0700

    first commit

---
+++ testclass4.py
@@ -0,0 +1,9 @@
+#use Auto matice massage
+print('hello!')
+x=input("what is your name")#ad rouls user
+print("nice too meet you"+x)
+a=int(input("enter a 1st angle"))
+b=int(input("enter a 2st angle"))
+c=int(input("enter a 3st angle"))
+d=a+b+c
+print(d)
